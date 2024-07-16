adedb8a29fa002711eb590e779533e741fb73e71
Enes Türk <partuminfernum@gmail.com>
2024-07-16 17:05:40 +0300

    detailed main.py, basic structure is on scraper.py

--- basic_global_scraper/main.py
+++ basic_global_scraper/main.py
@@ -1,42 +1,62 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 import csv
 
+
 def scrape_website(url):
-    response = requests.get(url) # GET request to the URL
+  response = requests.get(url)
+
+  if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
     
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        paragraphs = soup.find_all('p')
-        data = [p.text for p in paragraphs]
-        return data
-    else:
-        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
-        return None
+    orders = []
+    elements = []
+    types = []
+    contents = []
+    
+    for i, element in enumerate(soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'])):
+        tag_name = element.name
+        
+        element_type = 'header' if tag_name.startswith('h') else 'paragraph'
+        
+        elements.append(tag_name)
+        types.append(element_type)
+        contents.append(element.get_text(strip=True))
+        orders.append(i + 1)
+    
+    df = pd.DataFrame({
+        'Order': orders,
+        'Element': elements,
+        'Type': types,
+        'Content': contents
+    })
+    
+    return df
+  else:
+    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+    return None
 
 def save_to_csv(data, filename):
-    with open(filename, 'w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Paragraph Text'])  # header
-        for item in data:
-            writer.writerow([item])
-
-def ask_input():
-  url = input("Enter the URL you want to scrape: ")
-  return url
+  with open(f'scrapes/{filename}', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Element', 'Type', 'Content', 'Order'])  # header
+    for index, row in data.iterrows():
+      writer.writerow(row)
 
 def main():
-    url = str(ask_input())
+  url = "https://www.example.com/"  # Replace with the URL you want to scrape
+  result = scrape_website(url)
+  
+  print("Starting web scraping...")
+  scraped_data = scrape_website(url)
+  
+  if scraped_data is not None:
     filename = "scraped_data.csv"
-    
-    print("Starting web scraping...")
-    scraped_data = scrape_website(url)
-    
-    if scraped_data:
-        save_to_csv(scraped_data, filename)
-        print(f"Data successfully scraped and saved to {filename}")
-    else:
-        print("Scraping failed.")
+    save_to_csv(scraped_data, filename)
+    print(f"Data successfully scraped and saved to {filename}")
+  else:
+    print("Scraping failed.")
 
 if __name__ == "__main__":
-    main()
+  main()
